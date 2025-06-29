4ef67362873acb149211b7eaa773612489e5b2a7
Bamlak <bamlakbee.12@gmail.com>
2025-06-29 22:16:36 +0300

    Modified book store

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -30,7 +30,7 @@ CREATE TABLE IF NOT EXISTS Customers(
 CREATE TABLE IF NOT EXISTS Orders(
     order_id INT PRIMARY KEY,
     customer_id INT,
-    FOREIGN KEY (customer_id) REFERENCES customers(customer_id),
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id),
     order_date DATE);
 
 
@@ -38,9 +38,9 @@ CREATE TABLE IF NOT EXISTS Orders(
 CREATE TABLE IF NOT EXISTS Order_Details(
     order_detail_id INT PRIMARY KEY,
     order_id INT,
-    FOREIGN KEY (order_id) REFERENCES orders(order_id),
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
     book_id INT,
-    FOREIGN KEY (book_id) REFERENCES books(book_id),
+    FOREIGN KEY (book_id) REFERENCES Books(book_id),
     quantity DOUBLE
 );
 
